22e4588d84d971d317631513eda8c778e7fd3f00
Curtis Bollinger <curtis.bollinger@gmail.com>
2017-09-17 11:21:58 -0500

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BodyMassIndexComponent } from './body-mass-index/body-mass-index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyMassIndexComponent
   ],
   imports: [
     BrowserModule
