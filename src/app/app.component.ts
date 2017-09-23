c218d7f430046f33b8cf86f8b626335ea6ed91bd
Curtis Bollinger <curtis.bollinger@gmail.com>
2017-09-22 19:10:31 -0500

    Working on main toolbar layout and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
-import { Component, trigger, state, transition, style, animate } from '@angular/core';
+import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'dc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('isHomeChanged', [
-      state('true' , style({ height: '*', opacity: 1, transform: 'scale(1.0)' })),
-      state('false', style({ height: '0', opacity: 0, transform: 'scale(0.0)'  })),
-      transition('1 => 0', animate('200ms')),
-      transition('0 => 1', animate('100ms'))
-    ])
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   route: string;
