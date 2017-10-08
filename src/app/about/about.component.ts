ce9f7f74a13e1965bbe4bf05dc110ef3620e6e64
Curtis Bollinger <curtis.bollinger@gmail.com>
2017-10-08 02:28:57 -0500

    minor about text changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,9 +11,9 @@ export class AboutComponent implements OnInit {
 
   aboutText: string[] = [
     `Dietetic Calculator is a tool created for dietitians to make common dietetics calculations quick and easy.
-    The tool is developed by Curtis Bollinger and dietetics knowledge comes from Chelsey Bollinger, RD, LD.`,
+    This tool is developed by Curtis Bollinger and dietetics knowledge is provided by Chelsey Bollinger, RD, LD.`,
 
-    `This tool is a passion project that we work on in our spare time.
+    `Dietetic Calculator is a passion project that we work on in our spare time.
     More calculators and other functionality will be added over time.`,
 
     `Please contact us if you have any suggestions for improvements.
