e77cc4378c4d4ff7a01965493fea5fdf5c590cc0
Curtis Bollinger <curtis.bollinger@gmail.com>
2017-10-17 13:20:42 -0500

    Still thinking about stepper or tabs

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -14,6 +14,7 @@ import {
   MatListModule,
   MatTooltipModule,
   MatStepperModule,
+  MatTabsModule,
 } from '@angular/material';
 
 @NgModule({
@@ -34,6 +35,7 @@ import {
     MatListModule,
     MatTooltipModule,
     MatStepperModule,
+    MatTabsModule,
   ]
 })
 export class MaterialModule { }
