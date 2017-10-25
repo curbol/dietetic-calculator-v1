2626338f94b9501ccc594c316ac8f1f68b6417a4
Curtis Bollinger <curtis.bollinger@gmail.com>
2017-10-25 17:01:34 -0500

    Working on saving calc tool settings in url path

--- src/app/calculator/calc.ts
+++ src/app/calculator/calc.ts
@@ -76,7 +76,7 @@ export module Calc {
     const distinctSelectionIds: Calc.Selection.Id[] = mergedSelectionIds.filter((v, i, a) => a.indexOf(v) === i);
 
     return distinctSelectionIds;
-  }
+  };
 
   export const getInputIds = (calcs: Calc.Calc[]) => {
     if (!calcs || calcs.length <= 0) {
@@ -87,16 +87,79 @@ export module Calc {
     const distinctInputIds: Calc.Input.Id[] = mergedInputIds.filter((v, i, a) => a.indexOf(v) === i);
 
     return distinctInputIds;
-  }
+  };
 
   export const inputConversion = (input: Calc.Input) => (targetSymbol: Unit.Symbol) => {
     const targetUnit = input.group.find(u => u.symbol === targetSymbol);
     return Unit.conversion(input.unit.factor)(targetUnit.factor)(input.value);
-  }
+  };
 
   export const getActiveDataCount = (data: Calc.Data[]): number => data.filter(d => d.active).length;
-  export const getAllActiveDataCount = (data: (Calc.Input[]|Calc.Selection[]|Calc.Data[])[]): number => getActiveDataCount([].concat.apply([], data));
+  export const getAllActiveDataCount = (data: (Calc.Input[]|Calc.Selection[]|Calc.Data[])[]): number =>
+    getActiveDataCount([].concat.apply([], data));
 
   export const getActiveFilledDataCount = (data: Calc.Data[]): number => data.filter(d => d.active && d.value).length;
-  export const getAllActiveFilledDataCount = (data: (Calc.Input[]|Calc.Selection[]|Calc.Data[])[]): number => getActiveFilledDataCount([].concat.apply([], data));
+  export const getAllActiveFilledDataCount = (data: (Calc.Input[]|Calc.Selection[]|Calc.Data[])[]): number =>
+    getActiveFilledDataCount([].concat.apply([], data));
+
+  export const toPath = (calcs: Calc[]) => (selections: Selection[]) => (inputs: Input[]): string => {
+    const stubs: string[] = [];
+
+    const activeCalculators: Calc[] = calcs.filter(c => c.active);
+    if (activeCalculators.length) {
+      const calcsStub = `c-${activeCalculators.map(c => Calc.Id[c.id]).join('-')}`;
+      stubs.push(calcsStub);
+    }
+
+    const activeFilledSelecitons: Selection[] = selections.filter(s => s.active && s.value);
+    if (activeFilledSelecitons.length) {
+      const selectionsStub = activeFilledSelecitons.map(s => `o-${Calc.Selection.Id[s.id]}-${Option.Id[s.value.id]}`).join(',');
+      stubs.push(selectionsStub);
+    }
+
+    const activeFilledSelections: Input[] = inputs.filter(i => i.active && i.value);
+    if (activeFilledSelections.length) {
+      const inputsStub = activeFilledSelections.map(i => `i-${Calc.Input.Id[i.id]}-${i.value}-${Unit.Symbol[i.unit.symbol]}`).join(',');
+      stubs.push(inputsStub);
+    }
+
+    return stubs.join(',');
+  };
+
+  const calcFromParts = (parts: string[]): Id[] => parts.map(p => Id[p]);
+
+  const selectionFromParts = (parts: string[]): {id: Selection.Id, valueId: Option.Id} => {
+    return {id: Selection.Id[parts[0]], valueId: Option.Id[parts[1]]};
+  };
+
+  const inputFromParts = (parts: string[]): {id: Input.Id, value: number, symbol: Unit.Symbol} => {
+    return {id: Input.Id[parts[0]], value: +parts[1], symbol: Unit.Symbol[parts[2]]};
+  };
+
+  export const fromPath = (path: string):
+  {calcs: Id[], selections: {id: Selection.Id, valueId: Option.Id}[], inputs: {id: Input.Id, value: number, symbol: Unit.Symbol}[]} => {
+    if (!path) { return; }
+
+    let calcs: Id[];
+    const selections: {id: Selection.Id, valueId: Option.Id}[] = [];
+    const inputs: {id: Input.Id, value: number, symbol: Unit.Symbol}[] = [];
+
+    const stubs: string[] = path.split(',');
+    stubs.forEach(stub => {
+      if (!stub) { return; }
+
+      const parts: string[] = stub.split('-');
+      const partsType: string = parts[0];
+      const partsData: string[] = parts.slice(1);
+      if (partsType === 'c') {
+        calcs = calcFromParts(partsData);
+      } else if (partsType === 'o') {
+        selections.push(selectionFromParts(partsData));
+      } else if (partsType === 'i') {
+        inputs.push(inputFromParts(partsData));
+      }
+    });
+
+    return { calcs, selections, inputs };
+  };
 }
--- src/app/calculator/tool/calculation-tool.component.ts
+++ src/app/calculator/tool/calculation-tool.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, OnChanges } from '@angular/core';
 import { ActivatedRoute, UrlSegment } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -17,33 +17,34 @@ import { Option } from '../option';
     appearOnTrue(),
   ]
 })
-export class CalculationToolComponent implements OnInit {
+export class CalculationToolComponent implements OnInit, DoCheck {
   system: Unit.System;
   calculators: Calc.Calc[] = [];
   inputs: Calc.Input[] = [];
   selections: Calc.Selection[] = [];
   baseUrl: string;
-  settings: string;
+  settingsPath: string;
+  previousPath: string;
 
-  calculatorsTitle = '1. Select Calculators';
-  calculatorsShortTitle = 'Calculators';
-  inputsTitle = '2. Input Data';
-  inputsShortTitle = 'Data';
-  outputsTitle = '3. View Results';
-  outputsShortTitle = 'Results';
+  readonly calculatorsTitle = '1. Select Calculators';
+  readonly calculatorsShortTitle = 'Calculators';
+  readonly inputsTitle = '2. Input Data';
+  readonly inputsShortTitle = 'Data';
+  readonly outputsTitle = '3. View Results';
+  readonly outputsShortTitle = 'Results';
 
-  missingInputsMessage = 'Some inputs are missing values.';
-  noCalculatorsSelectedMessage = 'No calculators are selected.';
+  readonly missingInputsMessage = 'Some inputs are missing values.';
+  readonly noCalculatorsSelectedMessage = 'No calculators are selected.';
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private calcService: CalculatorService,
     private location: Location,
   ) {
-    this.settings = activatedRoute.snapshot.params['settings'];
+    this.settingsPath = activatedRoute.snapshot.params['settings'];
 
     const url: UrlSegment[] = activatedRoute.snapshot.url;
-    this.baseUrl = (this.settings ? url.slice(0, -1) : url).join('/');
+    this.baseUrl = (this.settingsPath ? url.slice(0, -1) : url).join('/');
 
     this.system = Unit.System.metric;
     this.calculators = activatedRoute.snapshot.data['calculators'];
@@ -51,33 +52,36 @@ export class CalculationToolComponent implements OnInit {
     this.selections = activatedRoute.snapshot.data['selections'];
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.updateFromSettings();
+  }
 
-  getCalcURL = (): string => {
+  ngDoCheck() {
+    this.updatePathSettings();
+  }
+
+  calcToolSettingsToURL = (calcs: Calc.Calc[]) => (selections: Calc.Selection[]) => (inputs: Calc.Input[]): string => {
     const stubs: string[] = [];
 
-    if (this.getActiveCalculators().length) {
-      const calcsStub = `c-${this.getActiveCalculators().map(c => Calc.Id[c.id]).join('-')}`;
+    const activeCalculators: Calc.Calc[] = calcs.filter(c => c.active);
+    if (activeCalculators.length) {
+      const calcsStub = `c-${activeCalculators.map(c => Calc.Id[c.id]).join('-')}`;
       stubs.push(calcsStub);
     }
 
-    const activeFilledSelecitons: Calc.Selection[] = this.selections.filter(s => s.active && s.value);
+    const activeFilledSelecitons: Calc.Selection[] = selections.filter(s => s.active && s.value);
     if (activeFilledSelecitons.length) {
       const selectionsStub = activeFilledSelecitons.map(s => `o-${Calc.Selection.Id[s.id]}-${Option.Id[s.value.id]}`).join(',');
       stubs.push(selectionsStub);
     }
 
-    const activeFilledSelections: Calc.Input[] = this.inputs.filter(i => i.active && i.value);
+    const activeFilledSelections: Calc.Input[] = inputs.filter(i => i.active && i.value);
     if (activeFilledSelections.length) {
       const inputsStub = activeFilledSelections.map(i => `i-${Calc.Input.Id[i.id]}-${i.value}-${Unit.Symbol[i.unit.symbol]}`).join(',');
       stubs.push(inputsStub);
     }
 
-    const url = stubs.join(',');
-
-    this.location.replaceState(`${this.activatedRoute.snapshot.url[0]}/${url}`);
-
-    return this.location.normalize(url);
+    return stubs.join(',');
   }
 
   getActiveDataCount = (): number => Calc.getAllActiveDataCount([this.inputs, this.selections]);
@@ -92,11 +96,41 @@ export class CalculationToolComponent implements OnInit {
 
   allActiveResultsCompleted = (): boolean => this.getActiveCompletedResults().length === this.getActiveCalculators().length;
 
-  onActiveCalculatorsChanged(activeCalculators: Calc.Calc[]): void {
+  onActiveCalculatorsChanged = (activeCalculators: Calc.Calc[]): void => {
     const inputIdsToActivate: Calc.Input.Id[] = Calc.getInputIds(activeCalculators);
     const selectionIdsToActivate: Calc.Selection.Id[] = Calc.getSelectionIds(activeCalculators);
 
     this.inputs.forEach(input => input.active = inputIdsToActivate.includes(input.id));
     this.selections.forEach(selection => selection.active = selectionIdsToActivate.includes(selection.id));
+  }
+
+  private updatePathSettings = (): void => {
+    const path: string = Calc.toPath(this.calculators)(this.selections)(this.inputs);
+    if (path === this.previousPath) { return; }
+    this.previousPath = path;
+
+    console.log('path update');
+
+    const url: string = this.location.normalize(`${this.baseUrl}/${path}`);
+    this.location.replaceState(url);
+  }
+
+  private updateFromSettings = (): void => {
+    const dataSettings = Calc.fromPath(this.settingsPath);
+
+    dataSettings.calcs.map(id => this.calculators.find(c => c.id === id)).forEach(c => c.active = true);
+
+    dataSettings.selections.forEach(data => {
+      const selection = this.selections.find(s => s.id === data.id);
+      if (!selection) { return; }
+      selection.value = selection.group.find(o => o.id === data.valueId);
+    });
+
+    dataSettings.inputs.forEach(data => {
+      const input = this.inputs.find(i => i.id === data.id);
+      if (!input) { return; }
+      input.value = data.value;
+      input.unit = input.group.find(u => u.symbol === data.symbol);
+    });
   }
 }
